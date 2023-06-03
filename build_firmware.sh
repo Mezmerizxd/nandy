09c9f99e441ada67456197d637958a75ec0df0f5
Rory Pearson <52637194+Mezmerizxd@users.noreply.github.com>
2023-06-03 13:20:12 +0100

    Update build_firmware.sh

--- build_firmware.sh
+++ build_firmware.sh
@@ -1,2 +1,31 @@
-cmake ./Flasher -B ./build/Flasher
-make -C ./build/Flasher
+#!/bin/bash
+
+windows() {
+  cmake ./Flasher -B./build/Flasher -G "MinGW Makefiles"
+  mingw32-make -j4 -C ./build/Flasher
+  return
+};
+
+linux() {
+  cmake ./Flasher -B./build/Flasher
+  make -j4 -C ./build/Flasher
+  return
+};
+
+case "$(uname -s)" in
+
+   Linux)
+      echo "System is compatible, proceeding with build..."
+      linux
+     ;;
+
+   Darwin)
+      echo "MacOS not supported yet."
+      exit 1
+     ;;
+
+   CYGWIN*|MINGW32*|MINGW64*|MSYS*)
+      echo "System is compatible, proceeding with build..."
+      windows
+     ;;
+esac
